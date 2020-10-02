04dfe4bfd4664959eb9a63d0a9f97ec224882308
Prashik Patil <70844686+prashikpatil221@users.noreply.github.com>
2020-10-02 20:06:59 +0530

    created template for comp. coding

---
+++ prashik.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string.h>
+#include<algorithm>
+#include<cstdlib>
+#include<math.h>
+#include<map>
+#include<set>
+#include <numeric>
+#include <cstring>
+#include<unordered_set>
+#include <climits>
+using namespace std;
+
+
+
+int main() 
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		
+		cout<<endl;
+
+	}	
+	return 0;
+}
+
+
+// for comp. coding
